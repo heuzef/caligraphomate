4a0e9cfb8fb734cc1e67f8a0d83fb46e25eefd01
Heuzef <contact@heuzef.com>
2025-09-29 13:20:29 +0200

    Add callibrations-a5_cams

---
+++ imitations/imitation_callibration-a5_cams.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+RUN="record-callibration-a5_cams"
+PUSH_TO_HUB="False"
+EPISODE_TIME_S="60"
+RESET_TIME_S="60"
+NUM_EPISODES="1"
+##############################
+
+if [[ "$1" == "record" ]]; then
+    rm -rfv ~/.cache/huggingface/lerobot/$HF_USER/$RUN/
+    echo "Recording $RUN"
+    python -m lerobot.record \
+        --robot.type=so100_follower \
+        --robot.port=$PORT_FOLLOWER \
+        --robot.id=follower \
+        --robot.cameras="{ front: {type: opencv, index_or_path: 3, width: 1280, height: 720, fps: 30}}" \
+        --robot.cameras="{ front: {type: opencv, index_or_path: 6, width: 1280, height: 720, fps: 30}}" \
+        --teleop.type=so100_leader \
+        --teleop.port=$PORT_LEADER \
+        --teleop.id=leader \
+        --display_data=True \
+        --dataset.push_to_hub=$PUSH_TO_HUB \
+        --dataset.repo_id=$HF_USER/$RUN \
+        --dataset.episode_time_s=$EPISODE_TIME_S \
+        --dataset.reset_time_s=$RESET_TIME_S \
+        --dataset.num_episodes=$NUM_EPISODES \
+        --dataset.single_task="$RUN"
+
+elif [[ "$1" == "replay" ]]; then
+    echo "Replaying $RUN"
+    python -m lerobot.replay \
+        --robot.type=so100_follower \
+        --robot.port=$PORT_FOLLOWER \
+        --robot.id=follower \
+        --dataset.repo_id=$HF_USER/$RUN \
+        --dataset.episode=0
+
+else
+    echo "Need arguments for $RUN"
+    echo "record or replay"
+fi
