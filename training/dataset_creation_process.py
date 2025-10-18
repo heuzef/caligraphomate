7cc23b6940ea8322b412772c6c6819789bf5b471
esteban cosserat <esteban.cosserat@gmail.com>
2025-10-18 16:22:57 +0200

    refix

--- training/dataset_creation_process.py
+++ training/dataset_creation_process.py
@@ -104,13 +104,13 @@ def record_shape(shape):
         # Détermine si on push à la fin de l'épisode
         push = (i == total - 1)
 
-        # record_single_episode(shape, i, total, png_path, push=push) # TO UNCOMMENT
+        record_single_episode(shape, i, total, png_path, push=push) # TO UNCOMMENT
 
         if i < total - 1:
             action = wait_for_space_or_enter()
             if action == "push_quit":
                 # push dataset actuel
-                # record_single_episode(shape, i, total, png_path, push=True) # TO UNCOMMENT
+                record_single_episode(shape, i, total, png_path, push=True) # TO UNCOMMENT
                 return "quit"
 
     print(f"🚀 Dataset '{shape}' poussé sur Hugging Face !\n")
